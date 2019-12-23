3a17cf0d7dbf23aa2abfd801078a3ff7b2f1ea36
Thanh Vuong <thanhvg@gmail.com>
2019-12-22 22:55:13 -0700

    wip

--- autohide-mode-line.el
+++ autohide-mode-line.el
@@ -1,9 +1,10 @@
-(defun my-turn-on-mode-line-maybe ()
-  (when hidden-mode-line-mode
-    (hidden-mode-line-mode -1)))
+(defun my-turn-on-mode-line-maybe (buffer)
+  ;; (message "thanh on %s" (current-buffer))
+    (when hidden-mode-line-mode
+     (hidden-mode-line-mode -1)))
 
 (defun my-turn-on-mode-line ()
-  (run-with-idle-timer 1 nil #'my-turn-on-mode-line-maybe))
+  (run-with-idle-timer 3 nil #'my-turn-on-mode-line-maybe))
 
 (defun my-pre-command-hook-handle ()
   ;; (message "tahnh %s" this-command)
@@ -14,8 +15,15 @@
     (my-turn-on-mode-line)))
 
 (add-hook 'pre-command-hook #'my-pre-command-hook-handle)
+;; (add-hook 'pre-command-hook #'my-pre-command-hook-handle nil t)
+(remove-hook 'pre-command-hook #'my-pre-command-hook-handle)
+
+;; so when buffer is killed it should trigger this run on
+;; next select buffer to make sure mode-line is back
+;; without this hook, K on a symbol then q, mode-line is gone forever
+(add-hook 'kill-buffer-hook #'my-turn-on-mode-line)
 
 ;; (defun my-mode-line ()
 ;;   (hidden-mode-line-mode -1))
-
-;; (add-hook 'focus-out-hook  #'my-mode-line)
+;; (remove-hook 'focus-in-hook  #'my-mode-line)
+;; (remove-hook 'focus-out-hook  #'my-mode-line)
