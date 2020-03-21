268e8446870c79f6c827f5063d6f7796363655a2
Thanh Vuong <thanhvg@gmail.com>
2020-03-21 10:26:48 -0600

    add www persp

---
+++ pesp.el
@@ -0,0 +1,22 @@
+;; (spacemacs|define-custom-layout "@eww"
+;;   :binding "w"
+;;   :body
+;;   (progn
+;;     (defun spacemacs-layouts/add-eww-buffer-to-persp ()
+;;       (persp-add-buffer (current-buffer)
+;;                         (persp-get-by-name "@eww")))
+;;     (add-hook 'eww-mode-hook #'spacemacs-layouts/add-eww-buffer-to-persp)
+;;     (eww)))
+
+(defvar thanh/net-buffers '("*HN*" "*eww*" "*How Do You*"))
+
+(defun thanh/find-buffer (buffers)
+  (if-let (buf (seq-find #'get-buffer buffers))
+      buf
+    (get-buffer-create (car buffers))))
+
+(spacemacs|define-custom-layout "@www"
+  :binding "n"
+  :body
+  (switch-to-buffer (thanh/find-buffer thanh/net-buffers)))
+  
