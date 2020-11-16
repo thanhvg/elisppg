8a3b4df4cb84223db19887d720a6e9c892013753
Thanh Vuong <thanhvg@gmail.com>
2020-11-16 08:43:49 -0700

    wip

---
+++ spacemacs-key-bindings.el
@@ -0,0 +1,10 @@
+(which-key-add-keymap-based-replacements evil-normal-state-map (kbd "g s") "evil-easy-motion")
+
+(which-key-add-keymap-based-replacements global-map (kbd "g s") "evil-easy-motion")
+
+(which-key-add-keymap-based-replacements global-map (kbd "C-c C-w") "Save stuff")
+
+(which-key-add-keymap-based-replacements graphql-mode-map "g s" "evil-easy-motion")
+(which-key-add-keymap-based-replacements spacemacs-default-map (kbd "SPC g s" ) "evil-easy-motion")
+
+(which-key-add-keymap-based-replacements spacemacs-default-map (kbd "g s" ) "evil-easy-motion")
